f9c83f39b748817b2419dc7111dff37f4c212107
Tezan Sahu <tezansahu@gmail.com>
2020-08-06 20:23:29 +0000

    added functions for formats

---
+++ R/get.format.R
@@ -0,0 +1,55 @@
+##' Get details of PEcAn Format from the database using format id.
+##' Hits the `/api/formats/{format_id}` API endpoint.
+##' @name get.format
+##' @title Get details of PEcAn Model from the database using model id
+##' @param server Server object obtained using the connect() function 
+##' @param format_id ID of the model to retrieve
+##' @return Response obtained from the `/api/formats/{format_id}` endpoint
+##' @author Tezan Sahu
+##' @export
+##' @examples
+##' server <- connect(url="http://localhost:8000", username="carya", password="illinois")
+##' 
+##' # Get details of the Ameriflux.level4.h format (id = 19)
+##' res <- get.format(server, format_id=19)
+
+get.format <- function(server, format_id){
+  res <- NULL
+  tryCatch(
+    expr = {
+      url <- paste0(server$url, "/api/formats/", format_id)
+      
+      if(! is.null(server$username) && ! is.null(server$password)){
+        res <- httr::GET(
+          url,
+          httr::authenticate(server$username, server$password)
+        )
+      }
+      else{
+        res <- httr::GET(url)
+      }
+    },
+    error = function(e) {
+      message("Sorry! Server not responding.")
+    }
+  )
+  
+  if(! is.null(res)) {
+    
+    if(res$status_code == 200){
+      return(jsonlite::fromJSON(rawToChar(res$content)))
+    }
+    else if(res$status_code == 401){
+      stop("Invalid credentials")
+    }
+    else if(res$status_code == 404){
+      stop("Format not found")
+    }
+    else if(res$status_code == 500){
+      stop("Internal server error")
+    }
+    else{
+      stop("Unidentified error")
+    }
+  }
+}
---
+++ R/search.inputs.R
@@ -0,0 +1,79 @@
+##' Search PEcAn Input files from the database using search based on
+##' model_id & site_id Hits the `/api/inputs/` API endpoint with 
+##' relevant query parameters
+##' @name search.inputs
+##' @title Search for PEcAn Models from the database
+##' @param server Server object obtained using the connect() function 
+##' @param model_id ID of the model associated with the input, Default: NULL (Returns all models)
+##' @param site_id ID of the model associated with the input, Default: NULL (Returns all sites)
+##' @param offset The number of workflows to skip before starting to collect the result set.
+##' @param limit The number of workflows to return (Available values : 10, 20, 50, 100, 500)
+##' @return Response obtained from the `/api/inputs/` endpoint with relevant query parameters
+##' @author Tezan Sahu
+##' @export
+##' @examples
+##' server <- connect(url="http://localhost:8000", username="carya", password="illinois")
+##' 
+##' # Get details of all inputs
+##' res1 <- search.inputs(server)
+##' 
+##' # Get details of all inputs for the SIPNET ssr model (id = 1000000022)
+##' res2 <- search.inputs(server, model_id="1000000022")
+##' 
+##' # Get details of all inputs for the SIPNET ssr model (id = 1000000022) & Niwor Ridge site (id = 772)
+##' res3 <- search.inputs(server, model_id="1000000022", site_id="772")
+
+search.inputs <- function(server, model_id=NULL, site_id=NULL, offset=0, limit=50){
+  if(! is.element(limit, c(10, 20, 50, 100, 500))) {
+    stop("limit can be only one of 10, 20, 50, 100 or 500")
+  }
+  
+  url <- paste0(server$url, "/api/inputs/")
+  if(!is.null(model_id) && !is.null(site_id)){
+    url <- paste0(url, "?model_id=", model_id, "&site_id=", site_id, "&offset=", offset, "&limit=", limit)
+  }
+  else if(!is.null(model_id) && is.null(site_id)){
+    url <- paste0(url, "?model_id=", model_id, "&offset=", offset, "&limit=", limit)
+  }
+  else if(is.null(model_id) && !is.null(site_id)){
+    url <- paste0(url, "?site_id=", site_id, "&offset=", offset, "&limit=", limit)
+  }
+  else{
+    url <- paste0(url, "?offset=", offset, "&limit=", limit)
+  }
+  res <- NULL
+  tryCatch(
+    expr = {
+      if(! is.null(server$username) && ! is.null(server$password)){
+        res <- httr::GET(
+          url,
+          httr::authenticate(server$username, server$password)
+        )
+      }
+      else{
+        res <- httr::GET(url)
+      }
+    },
+    error = function(e) {
+      message("Sorry! Server not responding.")
+    }
+  )
+  
+  if(! is.null(res)) {
+    if(res$status_code == 200){
+      return(jsonlite::fromJSON(rawToChar(res$content)))
+    }
+    else if(res$status_code == 401){
+      stop("Invalid credentials")
+    }
+    else if(res$status_code == 404){
+      stop("Inputs not found")
+    }
+    else if(res$status_code == 500){
+      stop("Internal server error")
+    }
+    else{
+      stop("Unidentified error")
+    }
+  }
+}

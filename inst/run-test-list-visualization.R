fb540cee4a65542c501f79463b0b6a28a4aa52a4
theakhiljha <Akhilkumarjha9@gmail.com>
2021-06-23 18:45:45 +0530

    Updated scatter plot

--- inst/run-test-list-visualization.R
+++ inst/run-test-list-visualization.R
@@ -1,12 +1,43 @@
 source("run-test-list.R")
 library(plotly)
+library(tidyverse)
 
 view(stages)
 
+stages$site_id <- as.character(stages$site_id)
+stages$workflow_id <- as.character(stages$workflow_id)
+stages$model_id <- as.character(stages$model_id)
+stages$stage <- as.character(stages$stage)
 
-stages$site_id <- as.factor(stages$site_id)
-stages$workflow_id <- as.factor(stages$workflow_id)
-stages$model_id <- as.factor(stages$model_id)
+stages$success_status <-
+  ifelse(grepl("DONE", stages$stage), TRUE, FALSE)
+
+color_fun <-
+  ifelse(grepl("TRUE", stages$success_status), "red", "green")
+
+# Final Status Visualization Using Scatter Plot.
+
+scatter_plot <-
+  plot_ly(
+    data = stages,
+    y = ~ model_id,
+    x = ~ site_id,
+    text = ~ stage,
+    color = ~ success_status,
+    type = "scatter",
+    size = 10,
+    mode = "markers",
+    marker = list(
+      sizemode = "diameter",
+      opacity = 0.4,
+      color = color_fun
+    )
+  )
+scatter_plot %>% layout(
+  title = 'run-test-list.R Visualization',
+  xaxis = list(showgrid = FALSE),
+  yaxis = list(showgrid = FALSE)
+)
 
 # Final Status Visualization Using Bar Chart.
 
@@ -23,30 +54,6 @@ bar_plot <- stages %>%
   )
 bar_plot %>% layout(
   title = "run-test-list.R Visualization",
-  xaxis = list(showgrid = FALSE),
-  yaxis = list(showgrid = FALSE)
-)
-
-# Final Status Visualization Using Scatter Plot.
-
-scatter_plot <-
-  plot_ly(
-    data = stages,
-    y = ~ site_id,
-    x = ~ workflow_id,
-    text = ~ stage,
-    color = ~ site_id,
-    type = "scatter",
-    size = 10,
-    mode = "markers",
-    marker = list(
-      sizemode = "diameter",
-      opacity = 0.4,
-      colors = "Paired"
-    )
-  )
-scatter_plot %>% layout(
-  title = 'run-test-list.R Visualization',
   xaxis = list(showgrid = FALSE),
   yaxis = list(showgrid = FALSE)
 )

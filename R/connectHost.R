dc343ab2da51a9af1b5d377f2d95d21f25593bdc
Rohan Sasne <rohansasne30@gmail.com>
2023-06-16 15:28:16 +0530

    Create connectHost.R

---
+++ R/connectHost.R
@@ -0,0 +1,22 @@
+##' Creates a server object containing the base URL of the PEcAn API server, 
+##' the username & password, which can be simply passed into other functions 
+##' to make requests to the PEcAn API.
+##' Currently, there is no validation. In future, we may validate the username-password 
+##' pair by hitting the appropriate API endpoint
+##'
+##' @name connect
+##' @title Creates a server object that can be passed into other functions for making requests to the PEcAn API
+##' @param url Base URL of the PEcAn API Server
+##' @param username Username
+##' @param password Password corresponding to the username
+##' @return A server object that can be passed into other functions for making requests to the PEcAn API
+##' @export
+##' @examples
+##' server <- connect(url="http://localhost:8000", username="carya", password="illinois")
+
+connectHost <- function(url, username=NULL, password=NULL, host=NULL){
+  header <- add_headers(Host = host)
+  
+  res <- list(url=sub('^/|/$','',url), username=username, password=password, headers = header)
+  return(res)
+}
